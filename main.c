ee4c949e5e0cdda40bbee82560271f979bec820e
Arthur <pigeon.arthur@hotmail.fr>
2021-11-23 18:38:04 +0100

    oui

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include "libft.h"
 
 int main(void) {
-    char *s1 = "MZIRIBMZIRIBMZE123";
-	char *s2 = "MZIRIBMZE";
-    size_t n = strlen(s2);
+    char n[40] = "99999999999999999999999999";
+	int i1 = atoi(n);
+	int i2 = ft_atoi(n);
 
-    printf("%p\n", strnstr(s1, s2, n));
-    printf("%p\n", ft_strnstr(s1, s2, n));
+    printf("%d\n", i1);
+    printf("%d\n", i2);
     return (0);
 }
