b1fce8e367c790cced690232e1a50e551d155a3c
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-07-31 23:54:50 +0200

    update gnl REAL

--- src/addon/get_next_line.c
+++ src/addon/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 20:16:59 by apigeon           #+#    #+#             */
-/*   Updated: 2022/07/31 23:14:26 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/07/31 23:54:29 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,29 @@ static void	copy_until_end(char *src, char *dst)
 	dst[i] = 0;
 }
 
+static void	*free_strings(char *s1, char *s2)
+{
+	if (s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	return (NULL);
+}
+
 static char	*extract_line_and_save(char **line, char *save, int size)
 {
 	char	*tmp_save;
 
+	if (size < 0)
+		size = ft_strlen(save);
+	if (size == 0)
+		return (free_strings(save, NULL));
 	tmp_save = malloc(ft_strlen(save + size) + 1);
 	if (!tmp_save)
-		return (NULL);
+		return (free_strings(save, NULL));
 	copy_until_end(save + size, tmp_save);
 	*line = malloc(size + 1);
-	if (*line)
+	if (*line != NULL)
 	{
 		save[size] = 0;
 		copy_until_end(save, *line);
@@ -49,19 +62,16 @@ static char	*read_line(int fd, char **line, char *save)
 	char	*tmp;
 	char	*buff;
 
+	ret = BUFFER_SIZE;
 	buff = malloc(BUFFER_SIZE + 1);
 	if (!buff)
 		return (0);
 	tmp = ft_strchr(save, '\n');
-	while (tmp == NULL && ret != 0)
+	while (tmp == NULL && ret == BUFFER_SIZE)
 	{
 		ret = read(fd, buff, BUFFER_SIZE);
 		if (ret == -1)
-		{
-			free(buff);
-			free(save);
-			return (NULL);
-		}
+			return (free_strings(save, buff));
 		buff[ret] = 0;
 		tmp = ft_strjoin(save, buff);
 		free(save);
@@ -69,13 +79,13 @@ static char	*read_line(int fd, char **line, char *save)
 		tmp = ft_strchr(save, '\n');
 	}
 	free(buff);
-	return (extract_line_and_save(line, save, (save - tmp) + 1));
+	return (extract_line_and_save(line, save, (tmp - save) + 1));
 }
 
 char	*get_next_line(int fd)
 {
-	char	*line;
-	char	*save[OPEN_MAX];
+	char		*line;
+	static char	*save[OPEN_MAX] = {0};
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
